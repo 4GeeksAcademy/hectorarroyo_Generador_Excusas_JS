94ed58c6f68744bcf225368578d85d0c852bf680
HecOmAC <malinchecoatl@gmail.com>
2026-03-10 04:16:39 +0000

    refactor(buenasPracticas) Identacion y renombrar variables de archivos index.html y app.js

--- src/app.js
+++ src/app.js
@@ -5,31 +5,33 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
-
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird']
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
-function generadorDeExcusas(){
-
-let aleatorio = Math.floor(Math.random() * who.length);
-let aleatorio2 = Math.floor(Math.random() * action.length);
-let aleatorio3 = Math.floor(Math.random() * what.length);
-let aleatorio4 = Math.floor(Math.random() * when.length);
-
-return (who[aleatorio]+' '+action[aleatorio2]+' '+what[aleatorio3]+' '+ when[aleatorio4]);
-
-};
-
-generadorDeExcusas();
-let excusa = generadorDeExcusas();
-document.getElementById("excuse").innerHTML = excusa;
 
 
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = [
+    'before the class',
+    'when I was sleeping',
+    'while I was exercising',
+    'during my lunch',
+    'while I was praying'
+  ];
+
+  function generadorDeExcusas() {
+
+    let whoRandom = Math.floor(Math.random() * who.length);
+    let actionRandom = Math.floor(Math.random() * action.length);
+    let whatRandom = Math.floor(Math.random() * what.length);
+    let whenRandom = Math.floor(Math.random() * when.length);
+
+    return `${who[whoRandom]}${action[actionRandom]}${what[whatRandom]} ${when[whenRandom]}`;
+  }
+
+  let excusa = generadorDeExcusas();
+  document.getElementById("excuse").innerHTML = excusa;
 
 
 
